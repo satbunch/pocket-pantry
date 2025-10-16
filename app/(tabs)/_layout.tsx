79e6cf5ee8b7fd50f42a950c9f6b6b36d7ce7b4e
Satomi Yoshio <satomi.yoshio@diamond-f.co.jp>
2025-10-16 13:50:30 +0900

    add shopping_list, settting tab in tabbar.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -16,7 +16,23 @@ export default function TabsLayout() {
         options={{
           title: '在庫',
           tabBarLabel: '在庫',
-          headerTitle: '食材管理',
+          headerTitle: '在庫',
+        }}
+      />
+      <Tabs.Screen
+        name="shopping_list/index"
+        options={{
+          title: '買い物リスト',
+          tabBarLabel: '買い物リスト',
+          headerTitle: '買い物リスト',
+        }}
+      />
+      <Tabs.Screen
+        name="settings/index"
+        options={{
+          title: '設定',
+          tabBarLabel: '設定',
+          headerTitle: '設定',
         }}
       />
     </Tabs>

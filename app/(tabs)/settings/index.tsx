769470debd6cf2d0f9ef729ad1af13aa73b454e7
Satomi Yoshio <satomi.yoshio@diamond-f.co.jp>
2025-10-16 13:51:37 +0900

    formatting

--- app/(tabs)/settings/index.tsx
+++ app/(tabs)/settings/index.tsx
@@ -1,4 +1,4 @@
-import { Text } from "react-native";
+import { Text } from 'react-native';
 
 export default function SettingsScreen() {
   return <Text>設定</Text>;
--- app/(tabs)/shopping_list/index.tsx
+++ app/(tabs)/shopping_list/index.tsx
@@ -1,4 +1,4 @@
-import { Text } from "react-native";
+import { Text } from 'react-native';
 
 export default function ShoppingListScreen() {
   return <Text>買い物リスト</Text>;
